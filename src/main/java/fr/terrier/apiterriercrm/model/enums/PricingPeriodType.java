bcff506cc4a34b9237074768e9583f6cbd5163cb
lto <louistorbay@gmail.com>
2022-10-16 18:48:38 +0200

    Update holidays to mid season and update pricing accordingly

--- src/main/java/fr/terrier/apiterriercrm/model/enums/PricingPeriodType.java
+++ src/main/java/fr/terrier/apiterriercrm/model/enums/PricingPeriodType.java
@@ -3,13 +3,13 @@ package fr.terrier.apiterriercrm.model.enums;
 public enum PricingPeriodType {
     OFF_SEASON,
     PEAK_SEASON,
-    HOLIDAYS;
+    MID_SEASON;
 
     public String getLabel(Locale locale) {
         return switch (this) {
             case OFF_SEASON -> Locale.FR.equals(locale) ? "Basse saison" : "Off season";
             case PEAK_SEASON -> Locale.FR.equals(locale) ? "Haute saison" : "Peak season";
-            case HOLIDAYS -> Locale.FR.equals(locale) ? "Vacances scolaires" : "School holidays";
+            case MID_SEASON -> Locale.FR.equals(locale) ? "Mi saison" : "Mid season";
         };
     }
 }
