4cc52c22c8d22bb9ecac91e797f2bba38f7ba5b8
lto <louistorbay@gmail.com>
2023-07-04 12:20:52 +0200

    end icalendar url with .ics

--- src/main/java/fr/terrier/apiterriercrm/controller/CalendarController.java
+++ src/main/java/fr/terrier/apiterriercrm/controller/CalendarController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/public/calendar")
+@RequestMapping("/public/calendar.ics")
 @RequiredArgsConstructor
 public class CalendarController {
     private final CalendarService calendarService;
